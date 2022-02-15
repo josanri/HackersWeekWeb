af6e7170db5180f2a62f6a0cbfd8475de5086811
Rubén <rubenjr0@protonmail.com>
2022-02-15 19:38:00 +0100

    ✨ Footer base

---
+++ src/lib/footer.svelte
@@ -0,0 +1,18 @@
+<footer class="footer has-background-black has-text-light">
+    <div class="columns mx-4">
+        <div class="column has-text-left">
+            <b>Información</b>
+            <p>CEETSII <a href="/">correo</a></p>
+            <p>Universidad de Málaga</p>
+            <p>Ubicación</p>
+        </div>
+        <div class="column">
+            <b>Redes Sociales</b>
+            <p>Twitter @ceetsii</p>
+            <p>Instagram @ceetsii</p>
+        </div>
+        <div class="column">
+            <b>Sponsors</b>
+        </div>
+    </div>
+</footer>
