60ecd2f7ae945e4550b906524fb4ea86861c1a2a
Rubén <rubenjr0@protonmail.com>
2022-02-15 17:03:27 +0100

    🍻 Código de ejemplo

---
+++ src/lib/tarjeta.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let title, description, url;
+</script>
+
+<div class="box has-background-dark">
+    <h1 class="title has-text-primary">{title}</h1>
+    <p class="has-text-light">{description}</p>
+    {#if url !== undefined}
+    <a href={url} class="button">Inscripcion</a>
+    {/if}
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,14 +2,27 @@
 	export const prerender = true;
 </script>
 
+<script>
+	import datos from "$lib/datos.json";
+	import Tarjeta from "$lib/tarjeta.svelte";
+
+	const { conferencias } = datos;
+
+	console.log(conferencias);
+</script>
+
 <svelte:head>
 	<title>Home</title>
 </svelte:head>
 
 <section class="hero is-fullheight-with-navbar is-dark is-bold">
 	<div class="hero-body">
-	  <p class="title is-1">
-		Hackers Week 8
-	  </p>
+		<p class="title is-1">Hackers Week 8</p>
 	</div>
-  </section>
+</section>
+
+<section class="section">
+	{#each conferencias as c}
+		<Tarjeta title={c.title} description={c.description} url={c.url} />
+	{/each}
+</section>
