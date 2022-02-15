ed14b4abadb18e5dd6f1883cec7746f44436d58a
Rubén <rubenjr0@protonmail.com>
2022-02-15 19:27:47 +0100

    💄 Added links to header

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -1,21 +1,26 @@
-<nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+<nav class="navbar is-black" aria-label="main navigation">
     <div class="navbar-brand">
-        <a class="navbar-item" href="https://bulma.io">
-            <h1>Hackers</h1>
+        <a class="navbar-item" href="/">
+            <h1>HW&lt;&gt;8</h1>
         </a>
-        <div class="navbar-brand">
-            <!-- navbar items, navbar burger... -->
-            <a
-                role="button"
-                class="navbar-burger"
-                aria-label="menu"
-                aria-expanded="false"
-                data-target="navbarBasicExample"
-            >
-                <span aria-hidden="true" />
-                <span aria-hidden="true" />
-                <span aria-hidden="true" />
-            </a>
-        </div>
+        <!-- navbar items, navbar burger... -->
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <a
+            role="button"
+            class="navbar-burger"
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample"
+        >
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+        </a>
+    </div>
+    <div class="navbar-start">
+        <a class="navbar-item" href="#conferencias">Conferencias</a>
+        <a class="navbar-item" href="#talleres">Talleres</a>
+        <a class="navbar-item" href="#videojuegos">Videojuegos</a>
+        <a class="navbar-item" href="#rol">Rol</a>
     </div>
 </nav>
