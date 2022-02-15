906a73b02d10d83be110246823ad4456d5fe6dd1
José Manuel Sánchez Rico <jmsanchezrico@gmail.com>
2022-02-16 00:09:41 +0100

    💄 Cambiada la interfaz para móvil y escritorio

--- src/lib/tarjeta.svelte
+++ src/lib/tarjeta.svelte
@@ -9,24 +9,37 @@
 
 <div class="box has-background-dark">
     <div>
-        <div class="columns">
-            <div class="column">
-                <h1 class="title has-text-primary ">{title}</h1>
+        <div class="columns is-hidden-touch	">
+            <div class="column is-two-thirds">
+                <h1 class="title has-text-primary mb-2">{title}</h1>
             </div>
             <div class="column" />
             <p class="has-text-light has-text-right"><b>{date}</b></p>
+            </div>
         </div>
+        <div class="is-hidden-desktop">
+            <h1 class="title has-text-primary mb-0">{title}</h1>
+            <p class="has-text-light has-text-left"><b>Fecha: {date}</b></p>
+            <p class="has-text-light has-text-left">
+                <b>Ubicación</b>: {#if ubicacion == undefined}
+                    Salón de Actos.
+                {:else}
+                    {ubicacion}
+                {/if}
+            </p>
+        </div>
+
         {#if !open_desc}
             <div>
-                <p class="has-text-light is-info has-text-justified">
-                    {short_desc} 
-                    
+                <p class="has-text-light is-info has-text-justified mb-3">
+                    {short_desc}
+
                     ...
                     <span
                         class=" is-text is-small has-text-primary is-underlined is-clickable"
                         on:click={() => (open_desc = !open_desc)}
                     >
-                        {open_desc ? "Ver menos" : "Ver más"}
+                        {open_desc ? "<< Ver menos" : "Ver más >>"}
                     </span>
                 </p>
             </div>
@@ -38,14 +51,14 @@
                         class=" is-text is-small has-text-primary is-underlined is-clickable"
                         on:click={() => (open_desc = !open_desc)}
                     >
-                        {open_desc ? "Ver menos" : "Ver más"}
-                    </span>
+                    {open_desc ? "<< Ver menos" : "Ver más >>"}
+                </span>
                 </p>
             </div>
         {/if}
 
-        <br>
-        <div class="columns">
+
+        <div class="columns is-hidden-touch">
             <div class="column">
                 <p class="has-text-light has-text-left">
                     <b>Ubicación</b>: {#if ubicacion == undefined}
@@ -57,10 +70,22 @@
             </div>
             <div class="column" />
             {#if url !== undefined}
-                <a href={url} class="button is-primary  is-rounded"
+                <a href={url} class="button is-primary is-rounded"
                     >Inscríbete aquí</a
                 >
             {/if}
         </div>
+        <div class="is-hidden-desktop">
+            <div class="columns is-centered">
+            <div class="column has-text-centered">
+                {#if url !== undefined}
+                <a href={url} class="button buttadd is-primary is-rounded "
+                    >Inscríbete aquí</a
+                >
+                
+            {/if}
+            </div>
+        </div>
+        </div>
     </div>
-</div>
+  
