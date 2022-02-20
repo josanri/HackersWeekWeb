39813bedbcad9031cfec7ac0f842220e2faa66c6
Rubén <rubenjr0@protonmail.com>
2022-02-20 23:28:22 +0100

    💄 Página principal, layout y hero actualizados

--- src/lib/hero.svelte
+++ src/lib/hero.svelte
@@ -1,6 +1,5 @@
-<section class="hero is-medium is-black">
+<section class="hero is-fullheight-with-navbar is-black">
     <div class="hero-body">
         <h1 class="title is-1">HACKERS WEEK 8</h1>
-        <h3 class="subtitle ">Hemos vuelto</h3>
     </div>
 </section>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,23 +1,9 @@
-<script context="module">
-	export const prerender = true;
-</script>
-
 <script>
-	import datos from "$lib/datos.json";
 	import Hero from "$lib/hero.svelte";
-	import Tarjeta from "$lib/tarjeta.svelte";
-
-	const { conferencias } = datos;
 </script>
 
 <svelte:head>
-	<title>Home</title>
+	<title>Hackers Week 8</title>
 </svelte:head>
 
 <Hero />
-
-<section class="section">
-	{#each conferencias as c}
-		<Tarjeta evento={c} />
-	{/each}
-</section>
