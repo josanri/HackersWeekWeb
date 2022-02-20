e1a0f20e082e72f819bf3999eca06563aa762ac6
Rubén <rubenjr0@protonmail.com>
2022-02-20 23:26:13 +0100

    💩 Páginas Añadidas páginas de conferencias, talleres, videojuegos y rol

---
+++ src/routes/rol.svelte
@@ -0,0 +1,15 @@
+<script>
+    import datos from "$lib/datos.json";
+    import Tarjeta from "$lib/tarjeta.svelte";
+
+    const { rol } = datos;
+</script>
+
+<svelte:head>
+    <title>Rol</title>
+</svelte:head>
+
+<h3 class="title">Rol</h3>
+{#each rol as r}
+    <Tarjeta evento={r}/>
+{/each}
---
+++ src/routes/workshops.svelte
@@ -0,0 +1,15 @@
+<script>
+    import datos from "$lib/datos.json";
+    import Tarjeta from "$lib/tarjeta.svelte";
+
+    const { workshops } = datos;
+</script>
+
+<svelte:head>
+    <title>Talleres</title>
+</svelte:head>
+
+<h3 class="title">Talleres</h3>
+{#each workshops as t}
+    <Tarjeta evento={t} />
+{/each}
