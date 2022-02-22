c3b79d1c13ee74eda6c66756a6ae00a600511f75
Rubén <rubenjr0@protonmail.com>
2022-02-22 22:38:49 +0100

    🚑 Fixed home link (again)

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -37,7 +37,7 @@
 
 <nav class="navbar is-black" aria-label="main navigation">
     <div class="navbar-brand">
-        <a class="navbar-item" href="/{base}">
+        <a class="navbar-item" href="{base === '' ? '/' : base}">
             <h1>HW&lt;&gt;8</h1>
         </a>
         <!-- svelte-ignore a11y-missing-attribute -->
