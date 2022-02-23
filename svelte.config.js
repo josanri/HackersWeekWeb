aa4fff3141af8d3ad5511e3448280a83a90f5b77
Rubén <rubenjr0@protonmail.com>
2022-02-23 01:14:27 +0100

    🐛 Development base path fixed

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
     },
     
     paths: {
-      base: process.env.NODE_ENV === "production" ? "/HackersWeekWeb" : "/"
+      base: process.env.NODE_ENV === "production" ? "/HackersWeekWeb" : ""
     },
 
     vite: {
