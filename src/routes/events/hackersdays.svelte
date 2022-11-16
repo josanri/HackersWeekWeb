958749b8b40956b6be9b0085cbf60fab9b9091e1
José Manuel <jmsanchezrico@gmail.com>
2022-11-16 12:49:51 +0100

    💄 Añadido comentario sobre los Hackers Days y Dossier

--- src/routes/events/hackersdays.svelte
+++ src/routes/events/hackersdays.svelte
@@ -5,10 +5,20 @@
 
 <svelte:head>
     <title>Hackers Days</title>
-    <meta name="description" content="Información sobre los Hackers Days del curso 20222023." />
+    <meta name="description" content="Información sobre los Hackers Days del curso 2022-2023." />
 </svelte:head>
 
 <h3 class="title">Hackers Days</h3>
+<div class = "column is-narrow has-text-centered">
+    <div class="column box has-background-grey-dark has-text-light mt-4">
+        <p> Los Hackers Days son eventos mensuales realizados por el Consejo de Estudiantes de Informática con la colaboración de la Escuela Técnica Superior de Ingeniería Informática donde se reserva espacio y tiempo a ponentes y organizadores de talle para distintas actividades con una duración máxima de 1h y 45 minutos.</p>
+    </div>
+</div>
+<h4 class="title is-4 has-text-centered">Próximas Conferencias</h4>
 {#each ponencia as pon}
-    <Tarjeta evento={pon}/>
+<Tarjeta evento={pon}/>
 {/each}
+<div class="is-flex is-flex-direction-column has-text-centered">
+    <h4 class="title is-4 has-text-light">¿Quieres dar una ponencia o taller?</h4>
+    <a href = "https://forms.gle/eqH5rZDMYsRKeMzr6" class="button is-primary is-medium is-rounded">Contacta con nosotros</a>
+</div>
--- src/routes/info/aboutus.svelte
+++ src/routes/info/aboutus.svelte
@@ -2,14 +2,14 @@
     import conferences from "$lib/data/conferences.json";
     import Tarjeta from "$lib/tarjeta.svelte";
 </script>
-
 <svelte:head>
     <title>¿Qué es la Hackers Week?</title>
 	<meta name="description" content="Información sobre las conferencias de la Hackers Week 9." />
 </svelte:head>
 
 
-<div class="container pt-6">
+<section class="section">
+<div class="container">
 <h3 class="title">¿Qué es la Hackers Week?</h3>
 <p  class="box has-background-grey-dark has-text-light" >
     La Hackers Week es un evento <b>totalmente gratuito</b>, desarrollado por el Consejo de estudiantes de la ETSI Informática de la Universidad de Málaga. Durante todo el año, la organización del evento prepara las diferentes actividades a realizar para la próxima edición, además de estar presente en los cuatro días de duración de este.</p>
@@ -18,4 +18,8 @@
         <br>
         <p> Además de las actividades anteriormente mencionadas, se ponen a disposición espacios habilitados con videojuegos, juegos de mesa o juegos de rol y en los que también se realizan torneos y competiciones. </p>
     </div>
+    <div class="is-flex is-justify-content-center">
+        <iframe title="Dossier Hackers Week" src="https://drive.google.com/file/d/1f6kN6qjgKxThCh9vRxwkZ8VLWDETjmHQ/preview" width="640rem" height="840rem" allow="autoplay"></iframe>
+    </div>
 </div>
+</section>
