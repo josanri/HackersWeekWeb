99f6436cc3499b0be5d35b49abd81851edd27f49
José Manuel <jmsanchezrico@gmail.com>
2022-12-08 14:00:26 +0100

    💄 Layout para aboutus

--- src/routes/info/aboutus.svelte
+++ src/routes/info/aboutus.svelte
@@ -8,8 +8,6 @@
 </svelte:head>
 
 
-<section class="section">
-<div class="container">
 <h3 class="title">¿Qué es la Hackers Week?</h3>
 <p  class="box has-background-grey-dark has-text-light" >
     La Hackers Week es un evento <b>totalmente gratuito</b>, desarrollado por el Consejo de estudiantes de la ETSI Informática de la Universidad de Málaga. Durante todo el año, la organización del evento prepara las diferentes actividades a realizar para la próxima edición, además de estar presente en los cuatro días de duración de este.</p>
@@ -17,9 +15,7 @@
         <p> De esta forma, la Hackers Week acaba llegando año tras año para no solo los estudiantes del centro, sino para todo interesado en el sector, novedades y tecnologías de actualidad en todo el mundo en el ámbito de la informática. Cuenta con un amplio horario lleno de conferencias y talleres, ofreciendo la posibilidad a los asistentes de poder informarse sobre temas tan distintos como la seguridad informática, programación, investigación, videojuegos e inteligencia artificial. Todo esto traído a un horario de mañana como de tarde.</p>
         <br>
         <p> Además de las actividades anteriormente mencionadas, se ponen a disposición espacios habilitados con videojuegos, juegos de mesa o juegos de rol y en los que también se realizan torneos y competiciones. </p>
-    </div>
-    <div class="is-flex is-justify-content-center">
-        <iframe title="Dossier Hackers Week" src="https://drive.google.com/file/d/1f6kN6qjgKxThCh9vRxwkZ8VLWDETjmHQ/preview" width="640rem" height="840rem" allow="autoplay"></iframe>
-    </div>
 </div>
-</section>
+<div class="is-flex is-justify-content-center">
+    <iframe title="Dossier Hackers Week" src="https://drive.google.com/file/d/1f6kN6qjgKxThCh9vRxwkZ8VLWDETjmHQ/preview" width="640rem" height="840rem" allow="autoplay"></iframe>
+</div>
--- src/routes/info/horario.svelte
+++ src/routes/info/horario.svelte
@@ -5,52 +5,49 @@
     import Fila from "$lib/fila.svelte";
 </script>
 
-
 <svelte:head>
     <title>Horario</title>
     <meta name="description" content="Información sobre los horarios de conferencias, talleres y videojuegos de la Hackers Week 9."/>
 </svelte:head>
 
-<div class="container p-6 pt-6">
-    <h3 class="title">Horario</h3>
-    <h4 class ="title is-3">Conferencias y talleres</h4>
-    <table  class = "table">
-        <thead>
-            <tr>
-                <th class="has-text-light has-text-centered">
-                    Fecha
-                </th>
-                <th class="has-text-light has-text-centered">
-                    Título
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            {#each conferences as c}
-            <Fila evento={c} />
-            {/each}
-            {#each workshops as t}
-                <Fila evento={t} />
-            {/each}
-        </tbody>
-    </table>
-    <br>
-    <h4 class ="title is-3">Videojuegos</h4>
-    <table  class = "table is-striped is-hoverable">
-        <thead>
-            <tr>
-                <th class="has-text-light has-text-centered">
-                    Fecha
-                </th>
-                <th class="has-text-light has-text-centered">
-                    Título
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            {#each videogames as v}
-            <Fila evento={v} />
-            {/each}
-        </tbody>
-    </table>
-</div>
+<h3 class="title">Horario</h3>
+<h4 class ="title is-3">Conferencias y talleres</h4>
+<table  class = "table">
+    <thead>
+        <tr>
+            <th class="has-text-light has-text-centered">
+                Fecha
+            </th>
+            <th class="has-text-light has-text-centered">
+                Título
+            </th>
+        </tr>
+    </thead>
+    <tbody>
+        {#each conferences as c}
+        <Fila evento={c} />
+        {/each}
+        {#each workshops as t}
+            <Fila evento={t} />
+        {/each}
+    </tbody>
+</table>
+<br>
+<h4 class ="title is-3">Videojuegos</h4>
+<table  class = "table is-striped is-hoverable">
+    <thead>
+        <tr>
+            <th class="has-text-light has-text-centered">
+                Fecha
+            </th>
+            <th class="has-text-light has-text-centered">
+                Título
+            </th>
+        </tr>
+    </thead>
+    <tbody>
+        {#each videogames as v}
+        <Fila evento={v} />
+        {/each}
+    </tbody>
+</table>
