6599fa0fba0d1ae954ddd6e3ba9e3cf3aedf844e
José Manuel <jmsanchezrico@gmail.com>
2023-01-24 13:17:18 +0100

    ✨ Patrocinadores

---
+++ src/lib/patrocinador.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let patrocinador;
+    const {url, img, alt} = patrocinador;
+</script>
+
+<div class="has-text-centered m-6">
+          <a href={url}>
+            <figure class="image is-48x48">
+              <img src={img} alt={alt} width=240rem height=200rem>
+            </figure>
+          </a>
+</div>
